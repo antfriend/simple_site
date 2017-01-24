388dbbc1c97acbd45e6bebe5e973937b34952996
danray <dray@healthwise.org>
2017-01-24 16:31:56 -0700

    blarg

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var app = express();
 
-function myParentPath()
-{
-  //__dirname = "C:\Users\dray\Documents\GitHub\antfriend_site"
-  return "C:\\Users\\dray\\Documents\\GitHub\\";
+function myPublicPath() {
+  //__dirname = "current location"
+  return __dirname + "\\ public"; //assuming a public folder at the root
 }
 
 console.log('starting');
-var dir2use = myParentPath() + 'antfriend.github.io';
+var dir2use = myPublicPath();
 console.log(dir2use);
 
 app.use(express.static(dir2use));
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
 });
